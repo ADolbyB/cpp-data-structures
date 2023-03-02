60980a1831138232128b18a4b17c908be72c8f70
Joel Brigida <joel@drtechreps.com>
2023-03-01 21:31:22 -0500

    Add Array Sort Algorithm PseudoCode Ex

---
+++ DataStructures/Examples/SortArrays.cpp
@@ -0,0 +1,30 @@
+/**
+ * Joel Brigida
+ * COP3530: Data Structures
+ * Pseudocode for an array sort algorithm
+ * Try to Solve It and Use It.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    // A is the array
+    // key is the item to be inserted into the array
+
+    // INSERTION-SORT(A)
+    // for i = 1 to n
+  	// key = A [i]
+   	// j = i – 1
+    //    while j > = 0 and A[j] > key
+  	// 	A[j+1] = A[j]
+  	// 	j = j – 1
+  	// End while
+  	// A[j+1] = key
+    // End for
+
+    return 0;
+
+}
