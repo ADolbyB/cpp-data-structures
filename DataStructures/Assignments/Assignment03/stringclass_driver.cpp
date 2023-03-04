a62f0c6de71a0fa9700a0130ef948705bfccd881
Joel Brigida <joel@drtechreps.com>
2023-03-03 21:32:50 -0500

    Edit Driver Readability

--- DataStructures/Assignments/Assignment03/stringclass_driver.cpp
+++ DataStructures/Assignments/Assignment03/stringclass_driver.cpp
@@ -22,64 +22,67 @@ int main()
     string_class s;
 
 	cout << "****************************************" << endl
-		<< "Test#1: tesing default constructor and overloaded operator<< with chaining\n"
-		<< s << "1st blank line" << endl << s << "2nd blank line" << endl
-		<< "Test#1 Ended" << endl
-		<< "****************************************" << endl;
+		 << "Test#1: tesing default constructor and overloaded operator<< with chaining" << endl
+		 << s << "1st blank line" << endl << s << "2nd blank line" << endl
+		 << "Test#1 Ended" << endl
+		 << "****************************************" << endl;
 	string_class r("hello");
 	cout << "****************************************" << endl
-		<< "Test#2: tesing explicit-value constructor and overloaded operator<< with chaining\n"
-		<< r << endl << "1st blank line" << endl << r <<endl<< "2nd blank line " << endl
-		<< "Test#2 Ended" << endl
-		<< "****************************************" << endl;
+		 << "Test#2: tesing explicit-value constructor and overloaded operator<< with chaining" << endl
+		 << r << endl << "1st blank line" << endl << r << endl << "2nd blank line " << endl
+		 << "Test#2 Ended" << endl
+		 << "****************************************" << endl;
 	cout << "****************************************" << endl
-		<< "Test#3: tesing palindrome\n"
-		<< "****************************************" << endl;
+		 << "Test#3: tesing palindrome" << endl
+		 << "****************************************" << endl;
 	string response = "Y";
 	string ss;
-	while (response == "Y" || response == "y")
-	{
-		cout << "Enter String: ";
-		getline(cin, ss);
-		string_class main_string(ss);
 
-		if (main_string.palindrome())
+	while (response == "Y" || response == "y")
 		{
-			cout << ss << " is a palindrome\n";
+			cout << "Enter String: ";
+			getline(cin, ss);
+			string_class main_string(ss);
+
+			if (main_string.palindrome())
+				{
+					cout << ss << " is a palindrome" << endl;
+				}
+			else
+				{
+					cout << ss << " is not a palindrome" << endl;
+				}
+			cout << "Would you like to try another string? (Y or N):   ";
+			getline( cin,response);
 		}
-		else
-		{
-			cout << ss << " is not a palindrome\n";
-		}
-		cout << "Would you like to try another string? (Y or N):   ";
-		getline( cin,response);
-	}
+
 	cout << "Test#3 Ended" << endl
-		<< "****************************************" << endl;
+		 << "****************************************" << endl;
 	cout << "****************************************" << endl
-		<< "Test#4: tesing replace_all\n"
-		<< "****************************************" << endl;
+		 << "Test#4: tesing replace_all" << endl
+		 << "****************************************" << endl;
 	response = "y";
 	string current, old_substring, new_substring;
+
 	while (response == "Y" || response == "y")
-	{
-		cout << "Enter value for current_string: ";
-		getline(cin, current);
-		string_class current_string(current);
-		cout << "Enter old_substring: ";
-		getline(cin, old_substring);
-		cout << "Enter new_substring: ";
-		getline(cin, new_substring);
-		cout << "Current Value in Current string = " << current << endl;
-		current_string.replace_all(old_substring, new_substring);
-		cout << "New value in Current String = " << current_string << endl;
-		cout<< endl;
-		cout << "Would you like to try another string? (Y or N):   ";
-		getline(cin, response);
-		cout << endl;
-	}
+		{
+			cout << "Enter value for current_string: ";
+			getline(cin, current);
+			string_class current_string(current);
+			cout << "Enter old_substring: ";
+			getline(cin, old_substring);
+			cout << "Enter new_substring: ";
+			getline(cin, new_substring);
+			cout << "Current Value in Current string = " << current << endl;
+			current_string.replace_all(old_substring, new_substring);
+			cout << "New value in Current String = " << current_string << endl;
+			cout<< endl;
+			cout << "Would you like to try another string? (Y or N):   ";
+			getline(cin, response);
+			cout << endl;
+		}
 	cout << "****************************************" << endl
-		<< "End Test#4: tesing replace\n"
-		<< "****************************************" << endl;
+		 << "End Test#4: tesing replace" << endl
+		 << "****************************************" << endl;
 	return 0;
 }
