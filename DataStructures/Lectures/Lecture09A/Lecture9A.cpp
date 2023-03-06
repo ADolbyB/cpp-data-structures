9c430e3a3d38cda971ff9c175852b2400503e0da
Joel Brigida <joel@drtechreps.com>
2023-03-06 15:19:19 -0500

    Vector Data Structure Demo Edit

--- DataStructures/Lectures/Lecture09A/Lecture9A.cpp
+++ DataStructures/Lectures/Lecture09A/Lecture9A.cpp
@@ -17,58 +17,58 @@ search for a string match in a vector of strings.
 **********************************************************************/
 vector<string>::iterator search(vector<string>& s)
 {
-	vector<string>::iterator i = s.begin();
+    vector<string>::iterator i = s.begin();
 
-	return i;
+    return i;
 }
 
 int main()
 {
 
-	vector<string> s(10,"hello"); // declare string type vector
+    vector<string> s(10,"hello"); // declare string type vector
 
-	for(vector<string>::iterator i = s.begin(); i != s.end(); i++)
-	{
-		cout << "[x] = " << (*i) << endl;
-	}
+    for(vector<string>::iterator i = s.begin(); i != s.end(); i++)
+        {
+            cout << "[x] = " << (*i) << endl;
+        }
 
-	vector<int> I; // declare int type vector;
+    vector<int> I; // declare int type vector;
 
-	cout << endl << "(1) I.size() = " << I.size() << endl;
+    cout << endl << "(1) I.size() = " << I.size() << endl;
 
-	for (int i = 0; i < 10; i++)
+    for (int i = 0; i < 10; i++)
         {
             I.push_back(i);
             cout << "push_back(" << i << ") = " << I.size() << endl;
         }
 
-	cout << "(2) I.size() = " << I.size() << endl << endl;
+    cout << "(2) I.size() = " << I.size() << endl << endl;
 
-	for (int i = 0; i < 10; i++)
+    for (int i = 0; i < 10; i++)
         {
             cout << "(1) I[" << i << "] = " << I[i] << endl;
         }
 
-	cout << endl << endl;
+    cout << endl << endl;
 
     cout << "I.erase(I.begin());" << endl;
-	I.erase(I.begin());
+    I.erase(I.begin());
 
-	for (int i = 0; i < 9; i++)
+    for (int i = 0; i < 9; i++)
         {
             cout << "(2) I[" << i << "] = " << I[i] << endl;
         }
 
-	I.insert(I.begin() + 3, 222);
+    I.insert(I.begin() + 3, 222);
 
-	cout << endl << endl;
+    cout << endl << endl;
 
-	for (int i = 0; i < 10; i++)
+    for (int i = 0; i < 10; i++)
         {
             cout << "(3) I[i] = " << I[i] << endl;
         }
 
-	cout << endl << endl << (*(search(s))) << endl;
+    cout << endl << endl << (*(search(s))) << endl;
 
-	return 0;
+    return 0;
 }
