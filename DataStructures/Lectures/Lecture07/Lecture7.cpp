6e14f9d08abbdfb66d95a986cc8307f8aa1305a5
Joel Brigida <joel@drtechreps.com>
2023-03-06 15:10:45 -0500

    Reformatted Stack Class Overloaded Operator=

--- DataStructures/Lectures/Lecture07/Lecture7.cpp
+++ DataStructures/Lectures/Lecture07/Lecture7.cpp
@@ -25,10 +25,10 @@ class stack
 {
     public:
 
-        stack();                                            // default constructor
-        ~stack();                                           // destructor
-        stack(const stack& );                               // copy constructor
-        stack_element top();                                // checks if stack is full
+        stack();                            // default constructor
+        ~stack();                           // destructor
+        stack(const stack& );               // copy constructor
+        stack_element top();                // checks if stack is full
         void pop();
         void push(const stack_element& );
         void print();
@@ -40,77 +40,75 @@ class stack
 
 int main()
 {
-	stack S;
+    stack S;
 
-	S.push("Hello World!");
-	S.push("We Hope You Are Well");
-	S.push("Sooner Or Later...");
-	S.push("Goodbye, World!");
+    S.push("Hello World!");
+    S.push("We Hope You Are Well");
+    S.push("Sooner Or Later...");
+    S.push("Goodbye, World!");
 
-	S.print();
+    S.print();
 
-	cout << endl << endl;
-	S.print();
+    cout << endl << endl;
+    S.print();
 
-	cout << endl << endl;
-	S.pop();
-	S.print();
+    cout << endl << endl;
+    S.pop();
+    S.print();
 
-	cout << endl << endl;
+    cout << endl << endl;
 
-	stack E = S;	// Using Overloaded Assignment operator=
+    stack E = S;    // Using Overloaded Assignment operator=
 
-	E.print();
-	cout << endl << endl;
-	S.print();
+    E.print();
+    cout << endl << endl;
+    S.print();
 
-	return 0;
+    return 0;
 }
 
 stack::stack()
 {
     cout << "Inside Default Constructor" << endl;
-
     s_top = 0;
 }
+
 stack::~stack()
 {
-	cout << "Inside !~stack" << endl;
-	while(s_top != 0)
-		{
-			pop();
-		}
+    cout << "Inside !~stack" << endl;
+    while(s_top != 0)
+        {
+            pop();
+        }
 }
 
 void stack::pop()
 {
-	cout << "Inside pop" << endl;
-	stack_node *p;
+    cout << "Inside pop" << endl;
+    stack_node *p;
 
-	if (s_top != 0)
-		{
-			p = s_top;
-			s_top = s_top->next;
-			delete p;
-		}
+    if (s_top != 0)
+        {
+            p = s_top;
+            s_top = s_top->next;
+            delete p;
+        }
 }
 
 void stack::push(const stack_element& item)
 {
-	cout << "Inside push" << endl;
+    cout << "Inside push" << endl;
+    stack_node *p = new stack_node;
 
-	stack_node *p = new stack_node;
-
-	p->data = item;
-	p->next = s_top;
-	s_top = p;
+    p->data = item;
+    p->next = s_top;
+    s_top = p;
 }
 
 void stack::print()
 {
-	cout << "Call to print" << endl;
-
-	for(stack_node *p = s_top; p != 0; p = p->next)
+    cout << "Call to print" << endl;
+    for(stack_node *p = s_top; p != 0; p = p->next)
         {
             cout << p->data << endl;
         }
@@ -120,14 +118,13 @@ void stack::print()
 
 stack_element stack::top()
 {
-	cout << "Inside top" << endl;
-
-	if (s_top == 0)
+    cout << "Inside top" << endl;
+    if (s_top == 0)
         {
             cout << "WARNING: STACK FULL. Exiting..." << endl;
             exit(1);
         }
-	else
+    else
         {
             return s_top->data;
         }
@@ -135,13 +132,11 @@ stack_element stack::top()
 
 stack::stack(const stack& orig_obj)
 {
-	cout << "Inside the Copy Constructor" << endl;
+    cout << "Inside the Copy Constructor" << endl;
+    stack_node *p = orig_obj.s_top;
+    (*this).s_top = 0;
 
-	stack_node *p = orig_obj.s_top;
-
-	(*this).s_top = 0;
-
-	while(p != 0)
+    while(p != 0)
         {
             (*this).push(p->data); // stack is copied and stored in reverse order
             p = p->next;
